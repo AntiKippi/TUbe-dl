e5ac5ba0af9f164de9848e4f3696ab0fc6c721f5
AntiKippi <24373791+AntiKippi@users.noreply.github.com>
2023-10-04 17:46:20 +0200

    Initial commit

---
+++ TUbe-dl.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+######################################################
+# TUbe-dl
+# Utility to download all video items from TUbe (https://portal.tuwien.tv/)
+#
+# Author: Kippi
+# Version: 0.0.0
+######################################################
+
+
+def main():
+
+
+if __name__ == '__main__':
+    main()
